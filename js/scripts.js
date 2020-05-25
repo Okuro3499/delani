d2cb14f716c486b02935b61029bf11c0643c38d7
Gideon Okuro <gideonollonde@gmail.com>
2020-05-25 22:06:21 +0300

    add mail chimp API tracker

--- js/scripts.js
+++ js/scripts.js
@@ -31,80 +31,79 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
+$(document).ready(function() {
+    $("#work1").mouseover(function() {
+        $("#overlay").show();
+    }).mouseout(function() {
+        $("#overlay").hide();
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
+$(document).ready(function() {
+    $("#work2").mouseover(function() {
+        $("#overlay2").show();
+    }).mouseout(function() {
+        $("#overlay2").hide();
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
+$(document).ready(function() {
+    $("#work3").mouseover(function() {
+        $("#overlay3").show();
+    }).mouseout(function() {
+        $("#overlay3").hide();
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
+$(document).ready(function() {
+    $("#work4").mouseover(function() {
+        $("#overlay4").show();
+    }).mouseout(function() {
+        $("#overlay4").hide();
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#work5").mouseover(function(){
-      $("#overlay5").show();
-    }).mouseout(function(){
-      $("#overlay5").hide();
+$(document).ready(function() {
+    $("#work5").mouseover(function() {
+        $("#overlay5").show();
+    }).mouseout(function() {
+        $("#overlay5").hide();
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#work6").mouseover(function(){
-      $("#overlay6").show();
-    }).mouseout(function(){
-      $("#overlay6").hide();
+$(document).ready(function() {
+    $("#work6").mouseover(function() {
+        $("#overlay6").show();
+    }).mouseout(function() {
+        $("#overlay6").hide();
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#work7").mouseover(function(){
-      $("#overlay7").show();
-    }).mouseout(function(){
-      $("#overlay7").hide();
+$(document).ready(function() {
+    $("#work7").mouseover(function() {
+        $("#overlay7").show();
+    }).mouseout(function() {
+        $("#overlay7").hide();
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#work8").mouseover(function(){
-      $("#overlay8").show();
-    }).mouseout(function(){
-      $("#overlay8").hide();
+$(document).ready(function() {
+    $("#work8").mouseover(function() {
+        $("#overlay8").show();
+    }).mouseout(function() {
+        $("#overlay8").hide();
     });
-  });
+});
 
-  $(document).ready(function() {
-      $("form#form3").submit(function(event){
+$(document).ready(function() {
+    $("form#form3").submit(function(event) {
         var name = $("input#MERGE1").val();
         var email = $("input#MERGE0").val();
         var message = $("textarea#comment").val();
-        if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-          alert (name + ", we have received your message. Thank you for reaching out to us.");
+        if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
+            alert(name + ", we have received your message. Thank you for reaching out to us.");
+        } else {
+            alert("Please enter your name and email!");
         }
-        else {
-          alert("Please enter your name and email!");
-        }
-      });
-  });
+    });
+});
